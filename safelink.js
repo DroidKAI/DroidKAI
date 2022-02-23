0c745521c707e00cd13cd962e80b6e6782da97ce
DroidKAI <100287373+DroidKAI@users.noreply.github.com>
2022-02-23 09:31:52 -0300

    Add files via upload

---
+++ safelink.js
@@ -0,0 +1,10 @@
+<script type='text/javascript'>
+//<![CDATA[
+var setting = {
+	exceptionurl : "google.com,twitter.com",
+	path : "#?o="
+};
+//]]>
+</script>
+<script src='https://raw.githack.com/Bloggeradm/adm/master/Auto.js'/>
+<script src='https://www.blogger.com/feeds/2281072316856098043/posts/default?alt=json-in-script&amp;max-results=150&amp;callback=showurl'/>
